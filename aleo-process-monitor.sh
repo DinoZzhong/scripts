e32e6b631fbca39af90d956475c8cf096650dd64
DinoZzhong <zhong_hanjun@126.com>
2022-11-23 10:13:21 +0800

    日志优化
    1、写else判断
    2、增大允许的重试时间为15min

--- aleo-process-monitor.sh
+++ aleo-process-monitor.sh
@@ -23,7 +23,7 @@ log "开始检查"
 
 proc_name="snarkos"
 
-# 检查启动时间，启动时间过短则退出
+# 检查启动时间，启动时间过短则退出  15分钟内不作二次重启
 function check_process_runtime(){
   log "进入启动prover检查..."
   pid=`ps -ef|grep "$proc_name" |grep -v grep  |head -1|awk '{print $2}'`
@@ -33,7 +33,7 @@ function check_process_runtime(){
     if [[ ! -z $pid && -f /proc/$pid/stat ]]; then 
       up_time=$(cat /proc/$pid/stat | cut -d" " -f22)
       run_time=$((${sys_uptime%.*}-$up_time/$user_hz))
-      if [ $run_time -lt 300 ]; then 
+      if [ $run_time -lt 900 ]; then 
         log "进程执行时间 $run_time 秒，时间过短，本次不在重试，退出!!!"
         exit 0
       else
@@ -52,6 +52,7 @@ function run_aleo_prover(){
   log "开始启动执行"
   ## 暴力kill 
   ps -ef|grep "$proc_name" |grep -v grep  |awk '{print $2}' |xargs -r kill -9
+  sleep 3
   ## 加载环境变量重启 ß
   # source /root/.cargo/env
   # source /etc/profile
@@ -72,13 +73,15 @@ function check_error_log_restart(){
   log "日志检查 异常信息$1"
   nums=`tail -20 ~/snarkOS/run-prover.log |grep "$1" |wc -l `
   if [ $nums != 0 ];then
-   log "gerror.. [$1]"
-   ps -ef|grep snark |grep -v grep 
-   tail ~/snarkOS/run-prover.log
-   log "error detail " 
-   run_aleo_prover
- fi
- log "检测日志正常，跳过...当前输入字符串 $1"
+     log "gerror.. [$1]"
+     ps -ef|grep snark |grep -v grep 
+     tail ~/snarkOS/run-prover.log
+     log "error detail " 
+     run_aleo_prover
+  else 
+    log "检测日志正常，跳过...当前输入字符串 $1"
+  fi
+ 
 }
 
 # cpu 负载判断
@@ -87,12 +90,13 @@ function check_cpu_load_rate(){
   if [[ ! -z $cpu_load && $(echo "$cpu_load < 600" | bc ) = 1 ]]; then 
     # cpu 负载小于期望值 重启 ，正常16core 负载在700~800之间
     # 建议crontab 10min以上，默认由系统自动重启
+    ps -aux |grep "$proc_name" |grep -v grep 
     log "cpu 负载$cpu_load ,触发重启操作"
     run_aleo_prover
+  else 
+    log "cpu 负载 $cpu_load 在预期内"
   fi
-  log "cpu 负载 $cpu_load 在预期内"
 }
-
 
 # 周期判断，定时重启
 function check_cycle_unit(){
@@ -102,8 +106,9 @@ function check_cycle_unit(){
     # 取模为0，每8小时重启
     log "判断重启周期，当前每 $1 小时重启一次，重启！！！"
     run_aleo_prover
+  else 
+    log "不符合周期重启逻辑，跳过"
   fi
-  log "不符合周期重启逻辑，跳过"
 }
 
 # 检查进程
@@ -111,9 +116,12 @@ function check_thread_exits(){
   nums=`ps -ef|grep "$proc_name" |grep -i aprivatekey|grep -v grep |wc -l `
   if [ $nums != 2 ];then
     log "进程数量$nums 不符合预期，重启！！！"
+    ps -ef|grep "$proc_name" |grep -i aprivatekey
     run_aleo_prover
+  else 
+    log "进程数量符合预期，跳过"
   fi
-  log "进程数量符合预期，跳过"
+  
 }
 
 main(){
